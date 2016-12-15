0706f9e15e2179c36158ee50d552f20aa51303f6
bitrex <bitrex@earthlink.net>
2016-12-15 01:14:26 -0500

    Create smart_ptr_allocator_policy.h
    Pool allocator policy for smart_ptr

---
+++ smart_ptr_allocator_policy.h
@@ -0,0 +1,34 @@
+#ifndef SMART_PTR_ALLOCATOR_POLICY_H_INCLUDED
+#define SMART_PTR_ALLOCATOR_POLICY_H_INCLUDED
+
+#include "uP_allocator.h"
+
+using namespace uP_allocator;
+
+template <typename T1, size_t N1, typename PolicyT1 = uP_allocator_policy<T1>,
+          typename TraitsT1 = object_traits<T1> >
+struct uP_smart_ptr_allocator_policy {
+ public:
+  uP_smart_ptr_allocator_policy() = default;
+
+  template <typename T_obj>
+  static inline T_obj* allocate(const T_obj* ptr) {
+    return new (_allocator<T_obj, N1, PolicyT1, TraitsT1>.allocate())
+        T_obj(ptr);
+  }
+
+  template <typename T_obj>
+  static inline void deallocate(T_obj* ptr) {
+    _allocator<T_obj, N1, PolicyT1, TraitsT1>.deallocate(ptr);
+  }
+
+ private:
+  template <typename T2, size_t N2, typename PolicyT2, typename TraitsT2>
+  allocator<T2, N2, PolicyT2, TraitsT2> const& _allocator() {
+    static allocator<T2, N2, PolicyT2, TraitsT2> _ptr_allocator =
+        allocator<T2, N2, PolicyT2, TraitsT2>();
+    return _ptr_allocator;
+  }
+};
+
+#endif  // SMART_PTR_ALLOCATOR_POLICY_H_INCLUDED

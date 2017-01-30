b04a981b1977ee1b38a064112ed9bcf199e3e7cb
bitrex <bitrex@earthlink.net>
2017-01-30 01:49:39 -0500

    Update uP_static_class.h

--- uP_static_class.h
+++ uP_static_class.h
@@ -1,69 +1,115 @@
 #ifndef UP_STATIC_CLASS_H
 #define UP_STATIC_CLASS_H
 
-#include <cstddef>
 #include <utility>
-#include <new>
+#include "uP_allocator.h"
 
 namespace uP_static_class {
 
-template <class TDerivedWrapper, class TDerived>
-class static_class {
- public:
-  operator TDerived() const {
-    return &static_cast<static_class*>(this)->get_ref();
-  }
-
-  operator TDerived*() { return &static_cast<static_class*>(this)->get_ref(); }
-
- protected:
-  template <typename... Args1>
-  static_class(Args1&&... args)
-      : _derived(make_ref(std::forward<Args1>(args)...)) {}
-
-  TDerived& get_ref() { return _derived; }
-
- private:
-  TDerived& _derived;
-
-  static char* const buf_init() {
-    static char* _buf = new char[sizeof(TDerived)];
-    return _buf;
-  }
-
-  template <typename... Args2>
-  static TDerived& make_ref(Args2&&... args) {
-    struct init {
-     public:
-      init(Args2&&... args)
-          : _ref(new (buf_init()) TDerived(std::forward<Args2>(args)...)) {}
-
-      operator TDerived*() const { return _ref; }
-
-     private:
-      TDerived* const _ref;
-    };
-
-    static init _init(std::forward<Args2>(args)...);
-    return *_init;
-  }
-};
-
-template <class TDerived>
+template <class TDerived, size_t MaxInstances = 1>
 class static_class_wrapper;
 
-template <class TDerived>
-using static_class_t = static_class_wrapper<TDerived>;
+template <class TDerived, size_t MaxInstances = 1>
+using static_class_t = static_class_wrapper<TDerived, MaxInstances>;
 
-template <class TDerived>
-class static_class_wrapper
-    : public static_class<static_class_wrapper<TDerived>, TDerived> {
+template <class TDerivedWrapper, size_t MaxInstances>
+class uP_static_class {
  public:
-  template <typename... Args>
-  static_class_wrapper(Args&&... args)
-      : static_class<static_class_wrapper<TDerived>, TDerived>(
-            std::forward<Args>(args)...) {}
+  virtual ~uP_static_class() {}
+
+ protected:
+  template <typename T>
+  using class_allocator_t = uP_allocator::allocator<T, MaxInstances>;
+
+  template <typename T>
+  static class_allocator_t<T>* const s_allocator() {
+    static auto _class_allocator = new class_allocator_t<T>();
+    return _class_allocator;
+  }
 };
 
+template <class TDerived, size_t MaxInstances>
+class static_class_wrapper
+    : public uP_static_class<static_class_wrapper<TDerived, MaxInstances>,
+                             MaxInstances> {
+ public:
+  template <class T>
+  struct _class_ptr_s;
+
+  /* This template is required to ensure calls to the copy
+    constructor do not invoke the forwarding constructor */
+
+  template <typename... Args,
+            typename = decltype(TDerived(std::declval<Args>()...))>
+
+  explicit static_class_wrapper(Args&&... args)
+      : _s_class_ptr(new (allocator<_class_ptr_s<TDerived>>()->allocate())
+                         _class_ptr_s<TDerived>(std::forward<Args>(args)...)) {}
+
+  static_class_wrapper(const static_class_wrapper& other)
+      : _s_class_ptr(new (allocator<_class_ptr_s<TDerived>>()->allocate())
+                         _class_ptr_s<TDerived>(*(other._s_class_ptr))) {}
+
+  friend void swap(static_class_wrapper& first,
+                   static_class_wrapper& second) noexcept {
+    using std::swap;
+    swap(first._s_class_ptr, second._s_class_ptr);
+  }
+
+  static_class_wrapper& operator=(static_class_wrapper other) {
+    swap(*this, other);
+    return *this;
+  }
+
+  ~static_class_wrapper() {
+    _s_class_ptr->~_class_ptr_s();
+    allocator<_class_ptr_s<TDerived>>()->deallocate(_s_class_ptr);
+  }
+
+  operator TDerived*() { return _s_class_ptr->_class_ptr; }
+
+  operator TDerived*() const { return _s_class_ptr->_class_ptr; }
+
+  TDerived* operator->() { return _s_class_ptr->_class_ptr; }
+
+  TDerived* operator->() const { return _s_class_ptr->_class_ptr; }
+
+  template <class T>
+  struct _class_ptr_s {
+   public:
+   
+     /* This template is required to ensure calls to the copy
+    constructor do not invoke the forwarding constructor */
+   
+    template <typename... Args, typename = decltype(T(std::declval<Args>()...))>
+
+    explicit _class_ptr_s(Args&&... args)
+        : _class_ptr(new (_buf) T(std::forward<Args>(args)...)) {}
+
+    _class_ptr_s(const _class_ptr_s<T>& other)
+        : _class_ptr(new (_buf) T(*(other._class_ptr))) {}
+
+    _class_ptr_s& operator=(const _class_ptr_s<T>& other) = default;
+
+    ~_class_ptr_s() { _class_ptr->~T(); }
+
+    T* const _class_ptr;
+
+   private:
+    char _buf[sizeof(T)];
+  };
+
+ private:
+  _class_ptr_s<TDerived>* _s_class_ptr;
+
+  template <typename T>
+  using class_allocator_t = uP_allocator::allocator<T, MaxInstances>;
+
+  template <typename T>
+  class_allocator_t<T>* allocator() {
+    return this->template s_allocator<T>();
+  }
+};
 }
+
 #endif  // UP_STATIC_CLASS_H

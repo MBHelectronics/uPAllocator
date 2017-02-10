0aa5ed1e988f87e545d9f7cd581796d994999a70
bitrex <bitrex@earthlink.net>
2017-02-10 16:05:29 -0500

    Update smart_ptr_allocator_policy.h

--- smart_ptr_allocator_policy.h
+++ smart_ptr_allocator_policy.h
@@ -1,3 +1,4 @@
+
 #ifndef SMART_PTR_ALLOCATOR_POLICY_H_INCLUDED
 #define SMART_PTR_ALLOCATOR_POLICY_H_INCLUDED
 
@@ -5,29 +6,32 @@
 
 using namespace uP_allocator;
 
-template <typename T1, size_t N1, typename PolicyT1 = uP_allocator_policy<T1>,
-          typename TraitsT1 = object_traits<T1> >
+template <template <typename, size_t, typename, typename> class Derived,
+          size_t N>
 struct uP_smart_ptr_allocator_policy {
  public:
   uP_smart_ptr_allocator_policy() = default;
 
-  template <typename T_obj>
-  static inline T_obj* allocate(const T_obj* ptr) {
-    return new (_allocator<T_obj, N1, PolicyT1, TraitsT1>.allocate())
+  template <typename T_obj, size_t N1 = N,
+            typename PolicyT1 = uP_allocator_policy<T_obj>,
+            typename TraitsT1 = object_traits<T_obj>>
+  static inline T_obj* allocate(const T_obj& ptr) {
+    return new (_allocator<T_obj, N1, PolicyT1, TraitsT1>()->allocate())
         T_obj(ptr);
   }
 
-  template <typename T_obj>
+  template <typename T_obj, size_t N1 = N,
+            typename PolicyT1 = uP_allocator_policy<T_obj>,
+            typename TraitsT1 = object_traits<T_obj>>
   static inline void deallocate(T_obj* ptr) {
-    _allocator<T_obj, N1, PolicyT1, TraitsT1>.deallocate(ptr);
+    _allocator<T_obj, N1, PolicyT1, TraitsT1>()->deallocate(ptr);
   }
 
  private:
   template <typename T2, size_t N2, typename PolicyT2, typename TraitsT2>
-  allocator<T2, N2, PolicyT2, TraitsT2> const& _allocator() {
-    static allocator<T2, N2, PolicyT2, TraitsT2> _ptr_allocator =
-        allocator<T2, N2, PolicyT2, TraitsT2>();
-    return _ptr_allocator;
+  static allocator<T2, N2, PolicyT2, TraitsT2>* const _allocator() {
+    static auto allocator_ptr = new allocator<T2, N2, PolicyT2, TraitsT2>();
+    return allocator_ptr;
   }
 };
 
